1a25c0dfe1d8a82e9217e3427c2b2dc51ca34f01
Ryan Gantt <ryan@zuttonet.com>
2011-10-22 23:53:31 -0600

    removed debug_backtrace, since it can blow up bigtime

--- japha.php
+++ japha.php
@@ -1,8 +1,5 @@
 <?
 spl_autoload_register( function ( $class ) {
-	set_error_handler( function() use ( $class ) {
-		print_r( debug_backtrace() );
-	});
 	$file = str_replace( "_", "", $class );
 	require_once dirname(__FILE__)."/{$file}.php";
 });
