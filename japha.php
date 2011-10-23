339c588e8ca46234f5af9fdea9998eb0a9d18119
Ryan Gantt <ryan@zuttonet.com>
2011-10-22 22:41:41 -0600

    removed some diagnostic output

--- japha.php
+++ japha.php
@@ -1,10 +1,8 @@
 <?
 spl_autoload_register( function ( $class ) {
 	set_error_handler( function() use ( $class ) {
-		echo "ERROR LOADING {$class}!\n";
 		print_r( debug_backtrace() );
 	});
 	$file = str_replace( "_", "", $class );
 	require_once dirname(__FILE__)."/{$file}.php";
-	echo "loaded {$class}\n";
 });
