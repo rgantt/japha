b064777542228a1f2b3b7502f6114e925bee2119
Ryan Gantt <ryan@zuttonet.com>
2011-10-26 00:02:54 -0600

    determine whether file exists at all

--- japha.php
+++ japha.php
@@ -1,5 +1,9 @@
 <?php
 spl_autoload_register( function ( $class ) {
 	$file = str_replace( "_", "", $class );
-	require_once dirname(__FILE__)."/{$file}.php";
+	if( file_exists( dirname(__FILE__)."/{$file}.php" ) ) {
+		include_once dirname(__FILE__)."/{$file}.php";
+	} else {
+		echo "could not load {$file}";
+	}
 });
